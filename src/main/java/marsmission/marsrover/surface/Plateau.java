8b686b5d0fe08981a5c89dc007e5834e05ed726e
chadrakh <chadrak@outlook.com>
2023-09-08 00:01:12 +0100

    Feat: Add Plateau Class

--- src/main/java/marsmission/marsrover/surface/Plateau.java
+++ src/main/java/marsmission/marsrover/surface/Plateau.java
@@ -1,2 +1,26 @@
-package marsmission.marsrover.surface;public class Plateau {
+package marsmission.marsrover.surface;
+
+public class Plateau {
+    private static int existingInstances = 0;
+    final int ID;
+    final int LENGTH;
+    final int WIDTH;
+
+    public Plateau(int length, int width) {
+        this.ID = generateId();
+        this.LENGTH = length;
+        this.WIDTH = width;
+    }
+
+    private int generateId() {
+        int newId = existingInstances;
+        existingInstances++;
+
+        return newId;
+    }
+
+    public void getDetails() {
+        System.out.println("ID: " + ID);
+        System.out.println("Area: " + LENGTH + " x " + WIDTH);
+    }
 }
