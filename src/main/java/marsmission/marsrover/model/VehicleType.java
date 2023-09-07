62509410dc462ac7332391060131c8f31f9ca790
chadrakh <chadrak@outlook.com>
2023-09-08 00:00:06 +0100

    Feat: Add VehicleType enum

--- src/main/java/marsmission/marsrover/model/VehicleType.java
+++ src/main/java/marsmission/marsrover/model/VehicleType.java
@@ -1,2 +1,29 @@
-package marsmission.marsrover.model;public class VehicleType {
+package marsmission.marsrover.model;
+
+public enum VehicleType {
+    AERIAL,
+    SURFACE,
+    SUBSURFACE,
+    UNDERWATER;
+
+    @Override
+    public String toString() {
+        switch (this) {
+            case AERIAL -> {
+                return "Aerial";
+            }
+            case SURFACE -> {
+                return "Surface";
+            }
+            case SUBSURFACE -> {
+                return "Sub-surface";
+            }
+            case UNDERWATER -> {
+                return "Underwater";
+            }
+            default -> {
+                return "Unknown";
+            }
+        }
+    }
 }
