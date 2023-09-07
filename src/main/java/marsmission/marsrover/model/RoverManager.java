a8b43cc7c11133a30e8f28b8e18762d2fe59ca9b
chadrakh <chadrak@outlook.com>
2023-09-07 23:59:16 +0100

    Feat: Add RoverManager Class

--- src/main/java/marsmission/marsrover/model/RoverManager.java
+++ src/main/java/marsmission/marsrover/model/RoverManager.java
@@ -1,2 +1,41 @@
-package marsmission.marsrover.model;public class RoverManager {
+package marsmission.marsrover.model;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class RoverManager {
+    private final Map<Integer, Rover> ROVER_MAP = new HashMap<>();
+
+    public void addRover(Rover rover) {
+        ROVER_MAP.put(rover.getId(), rover);
+    }
+
+    public Rover findById(int id) {
+        return ROVER_MAP.get(id);
+    }
+
+    public Rover findByCodeName(String codename) {
+        for (Rover rover : ROVER_MAP.values()) {
+            String expectedCodename = rover.getType().toString() + "Rover-" + rover.getId();
+
+            if (expectedCodename.equalsIgnoreCase(codename)) {
+                return rover;
+            } else {
+                throw new IllegalArgumentException("Rover instruction error. Target rover does not exist.");
+            }
+        }
+        return null;
+    }
+
+    public List<String> findAll() {
+        List<String> roverList = new ArrayList<>();
+
+        for (Rover rover : ROVER_MAP.values()) {
+            roverList.add(rover.getCodeName());
+        }
+
+        return roverList;
+    }
 }
