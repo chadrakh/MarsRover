1bdebb9b130a9dae9fd96425f85d3ef8cf923daa
chadrakh <chadrak@outlook.com>
2023-09-07 23:59:46 +0100

    Feat: Add Vehicle Class

--- src/main/java/marsmission/marsrover/model/Vehicle.java
+++ src/main/java/marsmission/marsrover/model/Vehicle.java
@@ -1,2 +1,60 @@
-package marsmission.marsrover.model;public class Vehicle {
+package marsmission.marsrover.model;
+
+import java.util.Arrays;
+
+public abstract class Vehicle {
+    private static int existingInstances = 0;
+    final int ID;
+    final VehicleType TYPE;
+    char direction;
+    int[] position;
+
+    public Vehicle(VehicleType type, char direction, int[] position) {
+        this.ID = generateId();
+        this.TYPE = type;
+        this.direction = direction;
+        this.position = position;
+    }
+
+    private int generateId() {
+        int newId = existingInstances;
+        existingInstances++;
+
+        return newId;
+    }
+
+    public void getDetails() {
+        System.out.println("Type: " + TYPE.toString());
+        System.out.println("ID: " + ID);
+        System.out.println("Start Position: " + direction + " " + Arrays.toString(position));
+    }
+
+    public int getId() {
+        return ID;
+    }
+
+    public VehicleType getType() {
+        return TYPE;
+    }
+
+    public char getDirection() {
+        return direction;
+    }
+
+    public void setDirection(char direction) {
+        this.direction = direction;
+    }
+
+    public int[] getPosition() {
+        return position;
+    }
+
+    private void setPosition(int[] position) {
+        this.position = position;
+    }
+
+    public void moveTo(char direction, int[] position) {
+        setDirection(direction);
+        setPosition(position);
+    }
 }
