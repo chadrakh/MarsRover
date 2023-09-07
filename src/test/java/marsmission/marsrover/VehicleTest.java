55f953f2d0dd650ebb46ed7a91c248e25b8cc8e3
chadrakh <chadrak@outlook.com>
2023-09-07 23:55:56 +0100

    Feat: Update VehicleTest Imports

--- src/test/java/marsmission/marsrover/VehicleTest.java
+++ src/test/java/marsmission/marsrover/VehicleTest.java
@@ -1,7 +1,6 @@
 package marsmission.marsrover;
 
 import marsmission.marsrover.model.Rover;
-import marsmission.marsrover.model.VehicleType;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
