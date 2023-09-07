e5064f24c5952ceac96564ecf37ba3f58bad624b
chadrakh <chadrak@outlook.com>
2023-09-07 23:57:35 +0100

    Feat: Update test data for RoverTest

--- src/test/java/marsmission/marsrover/RoverTest.java
+++ src/test/java/marsmission/marsrover/RoverTest.java
@@ -1,26 +1,24 @@
-package org.example;
+package marsmission.marsrover;
 
+import marsmission.marsrover.model.Rover;
+import marsmission.marsrover.model.VehicleType;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.*;
+
 class RoverTest {
-    private final Rover ROVER = new Rover(RoverType.SURFACE, "0 0");
+    private final Rover ROVER = new Rover('N', new int[]{0, 0});
 
     @Test
     public void generateCodeName_ReturnsCodeName_IfRoverIsInstantiated() {
-        String expected = ROVER.getType() + "-" + ROVER.getId();
+        String expected = ROVER.getType() + "Rover-" + ROVER.getId();
         String result = ROVER.getCodeName();
 
         assertEquals(expected, result);
     }
 
     @Test
-    public void setId_Increments_IfRoverIsInstantiated() {
-
-    }
-
-    @Test
     public void setDirection_SetsDirectionValue_IfNewDirectionIsValid() {
-
     }
 
     @Test
