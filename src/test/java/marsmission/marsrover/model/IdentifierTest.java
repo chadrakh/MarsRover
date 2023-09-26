27f11d09bedb4a11ea8f080a64dc605ec9d704e0
chadrakh <chadrak@outlook.com>
2023-09-26 19:57:53 +0100

    Test: Add Identifier Test

--- src/test/java/marsmission/marsrover/model/IdentifierTest.java
+++ src/test/java/marsmission/marsrover/model/IdentifierTest.java
@@ -1,6 +1,16 @@
 package marsmission.marsrover.model;
 
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 class IdentifierTest {
+    private final Identifier IDENTIFIER = new Identifier();
+    @Test
+    public void generateId_IncrementsId_WithEachCall() {
+        long firstId = IDENTIFIER.generateId();
+        long secondId = IDENTIFIER.generateId();
+
+        assertEquals(firstId + 1, secondId);
+    }
   
 }
